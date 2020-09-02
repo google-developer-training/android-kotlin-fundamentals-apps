393dbad0ddff56f47dcfe135c97f17c6143e11db
Jhansi <24260141+jtavva@users.noreply.github.com>
2020-09-02 16:09:05 -0700

    update deprecated Handler() constructor.

--- DessertClickerFinal/app/src/main/java/com/example/android/dessertclicker/DessertTimer.kt
+++ DessertClickerFinal/app/src/main/java/com/example/android/dessertclicker/DessertTimer.kt
@@ -48,6 +48,7 @@ class DessertTimer(lifecycle: Lifecycle) : LifecycleObserver {
      * or actions (known as [Runnable]s)
      */
     private var handler = Looper.myLooper()?.let { Handler(it) }
+    private var handler = Handler(Looper.getMainLooper())
     private lateinit var runnable: Runnable
 
     init {
@@ -65,20 +66,19 @@ class DessertTimer(lifecycle: Lifecycle) : LifecycleObserver {
             // postDelayed re-adds the action to the queue of actions the Handler is cycling
             // through. The delayMillis param tells the handler to run the runnable in
             // 1 second (1000ms)
-            handler?.postDelayed(runnable, 1000)
+            handler.postDelayed(runnable, 1000)
         }
 
         // This is what initially starts the timer
-        handler?.postDelayed(runnable, 1000)
+        handler.postDelayed(runnable, 1000)
 
         // Note that the Thread the handler runs on is determined by a class called Looper.
-        // In this case, no looper is defined, and it defaults to the main or UI thread.
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun stopTimer() {
         // Removes all pending posts of runnable from the handler's queue, effectively stopping the
         // timer
-        handler?.removeCallbacks(runnable)
+        handler.removeCallbacks(runnable)
     }
 }
