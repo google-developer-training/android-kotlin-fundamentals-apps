d3b4b8fa163ed2fb73cbad4f53eaba830af0f7d3
fluffybunny074 <rocrtaylor@gmail.com>
2020-12-23 14:44:34 -0600

    Update GameOverFragment.kt
    Update Android 4.1.1 Kotlin 1.4.21

--- AndroidTriviaNavigation/app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ AndroidTriviaNavigation/app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -32,8 +32,8 @@ class GameOverFragment : Fragment() {
         val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_over, container, false)
 
-        binding.tryAgainButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_gameOverFragment_to_gameFragment)
+        binding.tryAgainButton.setOnClickListener {
+            view?.findNavController()?.navigate(R.id.action_gameOverFragment_to_gameFragment)
         }
         return binding.root
     }
